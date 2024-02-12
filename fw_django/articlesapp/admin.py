534aa9a5e3c63d01bd569764befc4ce333d69665
Yakovlev Andrew <drozy69@gmail.com>
2024-02-12 16:17:12 +0300

    models added to admin-panel

--- fw_django/articlesapp/admin.py
+++ fw_django/articlesapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import AuthorModel, ArticleModel, CommentModel
+
 # Register your models here.
+admin.site.register(AuthorModel)
+admin.site.register(ArticleModel)
+admin.site.register(CommentModel)
