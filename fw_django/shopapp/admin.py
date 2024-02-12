1fa3c61e5b3b71313adda976030b70b30bd65b6b
Yakovlev Andrew <drozy69@gmail.com>
2024-02-12 16:55:33 +0300

    admin.py edited

--- fw_django/shopapp/admin.py
+++ fw_django/shopapp/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Client, Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock']
+    ordering = ['-price', 'stock']
+    list_filter = ['price']
+    search_fields = ['description']
+    search_help_text = 'Search by product description'
+
+
 admin.site.register(Client)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
