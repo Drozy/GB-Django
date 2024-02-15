103b6c3898e362e8af57598922ad3d8d83ba1479
Yakovlev Andrew <drozy69@gmail.com>
2024-02-15 16:49:12 +0300

    6 lesson - debug

--- fw_django/fw_django/settings.py
+++ fw_django/fw_django/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = [
     '192.168.111.62',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 # Application definition
 LOGGING = {
     'version': 1,
@@ -93,9 +97,11 @@ INSTALLED_APPS = [
     'hw1app',
     'articlesapp',
     'shopapp',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
